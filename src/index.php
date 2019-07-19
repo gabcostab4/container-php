ebdc06f9ec990ca86f24e7ef58deaa8e9a5f0fa5
gabcostab4 <gabriel.silva@before.com.br>
2019-07-19 17:03:51 -0400

    Add sql script

--- src/index.php
+++ src/index.php
@@ -2,18 +2,22 @@
 
 $value = "World";
 
-$db = new PDO('mysql:host=database;dbname=mydb;charset=utf8mb4', 'myuser', 'secret');
+try {
+	
+	$db = new PDO('mysql:host=database;port=3306;dbname=mydb;charset=utf8mb4', 'myuser', 'secret');
+	$databaseTest = $db->query('SELECT * FROM dockerSample')->fetchAll(PDO::FETCH_OBJ);
 
-$databaseTest = ($db->query('SELECT * FROM dockerSample'))->fetchAll(PDO::FETCH_OBJ);
+} catch(PDOException $e) {
+	die($e);
+}
 
 ?>
 
 <html>
-    <body>
-        <h1>Hello, <?= $value ?>!</h1>
-
-        <?php foreach($databaseTest as $row): ?>
-            <p>Hello, <?= $row->name ?></p>
-        <?php endforeach; ?>
-    </body>
+<body>
+	<h1>Hello, <?= $value ?>!</h1>
+	<?php foreach($databaseTest as $row): ?>
+		<p>Hello, <?= $row->name ?></p>
+	<?php endforeach; ?>
+</body>
 </html>
